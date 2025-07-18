b6b24dedc619fd70abc2ae6151838d10fd076ac2
danielcastro <danielcastro@jcmsystem.com.br>
2025-07-18 09:13:54 -0300

    feat(map): adiciona marcadores de início e fim da rota com ícones e tooltips

---
+++ src/components/RouteMarkers/RouteMarkers.tsx
@@ -0,0 +1,46 @@
+import { Marker, Tooltip } from "react-leaflet";
+import L from "leaflet";
+import RoomIcon from "@mui/icons-material/Room";
+import FlagIcon from "@mui/icons-material/Flag";
+import ReactDOMServer from "react-dom/server";
+
+interface RouteMarkersProps {
+  coords: [number, number][];
+}
+
+export default function RouteMarkers({ coords }: RouteMarkersProps) {
+  if (coords.length < 2) return null;
+
+  const startIcon = L.divIcon({
+    html: ReactDOMServer.renderToString(
+      <RoomIcon style={{ color: "#4caf50", fontSize: 36 }} />
+    ),
+    className: "",
+    iconSize: [36, 36],
+    iconAnchor: [18, 36],
+  });
+
+  const endIcon = L.divIcon({
+    html: ReactDOMServer.renderToString(
+      <FlagIcon style={{ color: "#f44336", fontSize: 36 }} />
+    ),
+    className: "",
+    iconSize: [36, 36],
+    iconAnchor: [18, 36],
+  });
+
+  return (
+    <>
+      <Marker position={coords[0]} icon={startIcon}>
+        <Tooltip direction="top" offset={[0, -20]} permanent={false}>
+          Ponto de Partida
+        </Tooltip>
+      </Marker>
+      <Marker position={coords[coords.length - 1]} icon={endIcon}>
+        <Tooltip direction="top" offset={[0, -20]} permanent={false}>
+          Destino Final
+        </Tooltip>
+      </Marker>
+    </>
+  );
+}
